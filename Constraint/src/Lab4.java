bc6dc882b6728f581d51f2f8df30aaf169a2dc45
Alexander Mjoberg <alexander.mjoberg@gmail.com>
2019-11-29 10:47:35 +0100

    Started lab4

---
+++ Constraint/src/Lab4.java
@@ -0,0 +1,23 @@
+import org.jacop.core.Store;
+
+public class Lab4 {
+	private Store store;
+	private int multiplicationDuration;
+	private int additionDuration;
+	private int numberOfMultiplicationOperations;
+	private int numberOfAdditionOperations;
+	private int totalNumberOfOperations;
+	private String[] typeOfOperation;
+	private int[] theLastNodes;
+	private String multiplication;
+	private String addition;
+	private int numberOfAdders;
+	private int numberOfMultipliers;
+	private int[][] edges;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
